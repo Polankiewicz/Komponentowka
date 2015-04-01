fe8a8007c4fd21e4ecf7cfe9b0a9ec63a77bb172
Polankiewicz <damianpol2@o2.pl>
2015-04-01 04:18:01 +0200

    zad3 tests v3

---
+++ Zadanie3/src/test/java/Zad3/Zadanie3/KsiegarniaTests.java
@@ -0,0 +1,92 @@
+package Zad3.Zadanie3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class KsiegarniaTests {
+
+Ksiegarnia ksiegarnia;
+	
+	@Before
+	public void init()
+	{
+		ksiegarnia = new Ksiegarnia();
+	}
+	
+	
+	@Test
+	public void testAppendElementAndSize() throws Exception {
+
+		int sud = ksiegarnia.size();
+		assertEquals(11, sud);
+
+		ksiegarnia.appendElement("XXX", "YYY", "ZZZ", 555);
+		sud = ksiegarnia.size();
+		assertEquals(12, sud);
+	}
+
+	@Test(expected=Exception.class)
+	public void testRemoveElementMinusValues() throws Exception {
+
+		ksiegarnia.removeElement(-2);
+	}
+
+	@Test(expected=Exception.class)
+	public void testRemoveElementOverRange() throws Exception {
+
+		ksiegarnia.removeElement(12);
+	}
+
+	@Test
+	public void testRemoveElementIndex() throws Exception {
+ 
+ 		ksiegarnia.removeElement(5);
+ 		assertEquals(10, ksiegarnia.size());
+ 	}
+ 
+	@Test
+	public void testGetElement() throws Exception {
+		
+		Ksiazka ksiazka1 = new Ksiazka("Kaznodzieja", "Camilla Laeckberg", "Kryminał", 15);
+		Ksiazka ksiazka2 = ksiegarnia.getElement(3);
+		assertEquals(ksiazka1.getTitle(), ksiazka2.getTitle());
+		assertEquals(ksiazka1.getAuthor(), ksiazka2.getAuthor());
+		assertEquals(ksiazka1.getGenre(), ksiazka2.getGenre());
+		assertEquals(ksiazka1.getISBN(), ksiazka2.getISBN());
+		
+	}
+
+	@Test(expected=Exception.class)
+	public void testGetElementMinusValue() throws Exception {
+		
+		Ksiazka ksiazka2 = ksiegarnia.getElement(-3);
+	}
+	
+	@Test(expected=Exception.class)
+	public void testGetElementOverRange() throws Exception {
+		
+		Ksiazka ksiazka2 = ksiegarnia.getElement(15);
+	}
+	
+	@Test
+	public void testSort() throws Exception {
+
+		ksiegarnia.print();
+		ksiegarnia.sort();
+		System.out.println();
+		ksiegarnia.print();
+	}
+	
+	@Test
+	public void testSortComparator() throws Exception {
+
+		ksiegarnia.print();
+		ksiegarnia.sort(new KomparatorISBN());
+		System.out.println();
+		ksiegarnia.print();
+		
+	}
+
+}
