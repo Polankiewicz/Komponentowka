f17bf42c09e58471e7fe1119f5248a6e790fbe86
Patrycja <Patrycja@Patrycja-PC>
2015-04-07 22:25:02 +0200

    zad5 tablice v1

--- Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/App.java
+++ Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/App.java
@@ -1,19 +1,24 @@
 package zad5.Zadanie5;
 
+import zad2.Zadanie2.Tablice;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
     	
         //String xxx = ReadWrite.readFromTextFile("plik.txt");
         //System.out.println( xxx );
         
+    	int[] tab;
+    	tab = Tablice.getTabWithRandValues(20);
         
         ReadWrite.writeToTextFile("Zapisanie tekstu do pliku",  "plik.txt");
+        ReadWrite.writeArrayToFile(tab, "tablice.bin");
         
     }
 }
--- Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/ReadWrite.java
+++ Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/ReadWrite.java
@@ -13,8 +13,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import Zad4.Zad4.Ksiazka;
-import Zad4.Zad4.Ksiegarnia;
+import zad2.Zadanie2.Tablice;
+
 
 public class ReadWrite {
 
@@ -25,7 +25,8 @@ public class ReadWrite {
 		
 		String tekstDoZapisania = text;
 
-		try	{
+		try	
+		{
 			plik.createNewFile();					// Utworzenie pliku pod sciezka zapisana w plik 
 			FileWriter strumienZapisu = new FileWriter(plik);	// Konstrukcja i otwarcie strumienia
 			strumienZapisu.write(tekstDoZapisania);		// Zapis do pliku, (tekstDoZapisania, int, int) - inty okreslaja ilosc znakow, ktore zostana zapisane
@@ -33,11 +34,14 @@ public class ReadWrite {
 		}
 
 		catch (IOException io)												
-			{System.out.println(io.getMessage());}
+		{
+			System.out.println(io.getMessage());
+		}
 
 		catch (Exception se)
-			{System.err.println("blad sec");}
-		
+		{
+			System.err.println("blad sec");
+		}
 	}
 	
 	public static String readFromTextFile(String file) // dodać polskie znaki w kodowaniu i zamknięcia buffora (w catch)
@@ -47,20 +51,22 @@ public class ReadWrite {
 		
 		FileReader fileReader;
 		
-		try {
+		try
+		{
 			fileReader = new FileReader(file);
 			
 			BufferedReader bufferedReader = new BufferedReader(fileReader); 	// Konstrukcja i otwarcie strumienia odczytujacego
 			  
 			String textLine;// = bufferedReader.readLine();
-			do {
+			do 
+			{
 			    		  
 			    textLine = bufferedReader.readLine();  // Odczytanie znakow od 0 do 7 ze strumienia do bufora
 			    
 			    if(textLine != null)
 			    	text += textLine + "\n";
-			} 
-			while(textLine != null);
+			    
+			} while(textLine != null);
 
 			bufferedReader.close();
 			
@@ -78,43 +84,56 @@ public class ReadWrite {
 		
 	}
 	
-	
+//	int zakres = 30;	
+//	int i; 
+//	int rozmiar = 15; 
+//	int tab[] = new int[rozmiar];
 	
 	public static void writeArrayToFile(int[] tab, String file)
 	{		
+		File plik = new File(file);
 		
-		Ksiegarnia ksiegarnia.ksiegarnia.print();
+//		int rozmiar = 0;
+		//int[] tab1;
+			
 		
-		
-		try { 
+		try 
+		{ 
+			//tab = Tablice.getTabWithRandValues(20);
+			
 			DataOutputStream strumienTablicy = new DataOutputStream(new FileOutputStream(file));// Strumien zapisujacy liczby  
 			for (int i=0; i< tab.length; i++) strumienTablicy.writeInt(tab[i]); 
 			strumienTablicy.close();
 		} 
-		catch (IOException io) {
+		catch (IOException io) 
+		{
 			System.out.println(io.getMessage());
 		} 
-		catch (Exception se) {
+		catch (Exception se) 
+		{
 			System.err.println("blad sec");
 		}
 	}
 	
 	public static int[] readArrayFromFile(String file)
 	{
-		int [] tab;
-		try {
+		int [] tablica = null;
+		try 
+		{
 			DataInputStream strumienTablicaZPliku = new DataInputStream(new FileInputStream(file)); 
-			for (int i=0; i< rozmiar; i++) tab[i] = strumienTablicaZPliku.readInt();
+			for (int i=0; i< tablica.length; i++) tablica[i] = strumienTablicaZPliku.readInt();
+			
+			
 			strumienTablicaZPliku.close();
 		} 
-		catch (FileNotFoundException io) {
+		catch (FileNotFoundException io) 
+		{
 			System.out.println(io.getMessage());
 		} 
-		catch (IOException io) {
+		catch (IOException io) 
+		{
 			System.out.println(io.getMessage());
 		}
-		return tab;
+		return tablica;
 	}
-	
-	
 }
