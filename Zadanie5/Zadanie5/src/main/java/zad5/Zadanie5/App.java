b0651b6c01daf502bc34dbdedcb603078bcbf6cd
Patrycja <patrycja.bubas@gmail.com>
2015-04-21 23:14:10 +0200

    poprawa strumienia

--- Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/App.java
+++ Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/App.java
@@ -12,10 +12,10 @@ public class App
         ReadWrite.writeToTextFile("Zapisanie tekstu do pliku \nęóąśłżźćń",  "plik.txt");
         System.out.println(ReadWrite.readFromTextFile("plik.txt"));
         
-        int[] tab =  Tablice.getTabWithRandValues(10);
+        int[] tab =  Tablice.getTabWithRandValues(12);
         System.out.println(Arrays.toString(tab)); // sie wywali potem
         ReadWrite.writeArrayToFile(tab, "tablice.bin");
-        System.out.println("odczyt tablicy: \n" + Arrays.toString( ReadWrite.readArrayFromFile("tablice.bin") ) );
+        ReadWrite.readArrayFromFile("tablice.bin");
         
     }
 }
--- Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/ReadWrite.java
+++ Zadanie5/Zadanie5/src/main/java/zad5/Zadanie5/ReadWrite.java
@@ -10,102 +10,139 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 public class ReadWrite {
 
 	private static Logger logger = Logger.getLogger(ReadWrite.class.getName());
-	
-	public static void writeToTextFile(String text, String fileLocation)
-	{
+
+	public static void writeToTextFile(String text, String fileLocation) {
 		File plik = new File(fileLocation);
 
-		try	(FileWriter strumienZapisu = new FileWriter(plik))
-		{
+		try (FileWriter strumienZapisu = new FileWriter(plik)) {
 			plik.createNewFile();
 			strumienZapisu.write(text);
-		}
-		catch (IOException io)
-		{
+		} catch (IOException io) {
 			logger.log(Level.INFO, "Wyjątek I/O ", io);
-		}
-		catch (Exception se)
-		{
+		} catch (Exception se) {
 			logger.log(Level.INFO, "Błąd sec ", se);
 		}
 	}
-	
-	public static String readFromTextFile(String file)
-	{
-		String text = "";
-		
-		try (FileReader fileReader = new FileReader(file); 
-				BufferedReader bufferedReader = new BufferedReader(fileReader))
-		{
-			String textLine;
-			
-			do 
-			{  
-			    textLine = bufferedReader.readLine();
-			    
-			    if(textLine != null)
-			    	text += textLine + "\n";
-			} 
-			while(textLine != null);
 
-		} 
-		catch (FileNotFoundException io) 
-		{
+	public static String readFromTextFile(String file) {
+		String text = "";
+
+		try (FileReader fileReader = new FileReader(file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+			String textLine;
+
+			do {
+				textLine = bufferedReader.readLine();
+
+				if (textLine != null)
+					text += textLine + "\n";
+			} while (textLine != null);
+
+		} catch (FileNotFoundException io) {
+			logger.log(Level.INFO, "Błąd sec", io);
+		} catch (IOException io) {
 			logger.log(Level.INFO, "Błąd sec", io);
 		}
-		catch (IOException io) 
-		{
-			logger.log(Level.INFO, "Błąd sec", io);
-		}
-		  
+
 		return text;
 	}
-	
-	public static void writeArrayToFile(int[] tab, String file)
-	{			
-		try (DataOutputStream strumienTablicy = new DataOutputStream(new FileOutputStream(file)))
-		{ 
-			for (int i=0; i< tab.length; i++) 
-				strumienTablicy.writeInt(tab[i]); 			
-		} 
-		catch (IOException io) 
-		{
-			logger.log(Level.INFO, "Wyjątek I/O" , io);
-		} 
-		catch (Exception se) 
-		{
-			logger.log(Level.INFO, "Błąd sec" , se);
+
+	public static void writeArrayToFile(int[] tab, String file) {
+		try (DataOutputStream strumienTablicy = new DataOutputStream(
+				new FileOutputStream(file))) {
+			for (int i = 0; i < tab.length; i++)
+				strumienTablicy.writeInt(tab[i]);
+		} catch (IOException io) {
+			logger.log(Level.INFO, "Wyjątek I/O", io);
+		} catch (Exception se) {
+			logger.log(Level.INFO, "Błąd sec", se);
 		}
 	}
-	
-	public static int[] readArrayFromFile(String file)
-	{
-		int [] tablica = new int [10]; // to trzeba poprawic, tymczasowy rozmiar!!!
-		// tablica o znacznie wiekszym rozmiarze niż potrzebny jest brzydka :P
-		// no i przy obecnym kodzie bedzie wywalać błędy bo odczytujemy ze strumienia coś co nie istnieje
+
+	public static int[] readArrayFromFile(String file) {
 		
-		try (DataInputStream strumienTablicaZPliku = new DataInputStream(new FileInputStream(file)))
+
+//		RandomAccessFile raf = new RandomAccessFile(file, "rw");
+//		rozm = raf.length();
+//
+//		Integer k = (int) (long) rozm;
+//
+//		int[] tablica = new int[k];
+
+		// to trzeba poprawic, tymczasowy rozmiar!!!
+		// tablica o znacznie wiekszym rozmiarze niż potrzebny jest brzydka :P
+		// no i przy obecnym kodzie bedzie wywalać błędy bo odczytujemy ze
+		// strumienia coś co nie istnieje
+
+//		try (
+//				DataInputStream strumienTablicaZPliku = new DataInputStream(
+//				new FileInputStream(file))) {
+//			for (int i = 0; i < tablica.length; i++)
+//				tablica[i] = strumienTablicaZPliku.readInt();
+//		} catch (FileNotFoundException io) {
+//			logger.log(Level.INFO, "Błąd pliku", io);
+//		} catch (IOException io) {
+//			logger.log(Level.INFO, "Wyjątek I/O", io);
+//		}
+
+		// return tablica;
+
+		RandomAccessFile fis = null;
+		int []tablica = null;
+		
+		// OTWIERANIE PLIKU:
+		try 
 		{
-			for (int i=0; i< tablica.length; i++) 
-				tablica[i] = strumienTablicaZPliku.readInt();
+			fis = new RandomAccessFile(file, "rw");
 		} 
-		catch (FileNotFoundException io) 
+		catch (FileNotFoundException e) 
 		{
+			System.out.println("BŁĄD!!! Nie znaleziono pliku...");
+			System.exit(1);
+		}
+		
+		
+		long rozm;
+		
+		//ODCZYT
+		try 
+		{
+			rozm = fis.length();
+			
+			Integer k = (int) (long) rozm / 4;
+			
+			tablica = new int[k];
+			
+			for (int i = 0; i < k; i++)
+			{
+				tablica[i] = fis.readInt();
+				System.out.println("Tablica z pliku: " + tablica[i] + " ");
+			}
+		} 
+		catch (FileNotFoundException io) {
 			logger.log(Level.INFO, "Błąd pliku", io);
-		} 
-		catch (IOException io) 
-		{
+		} catch (IOException io) {
 			logger.log(Level.INFO, "Wyjątek I/O", io);
+		}
+		
+		
+		// ZAMYKANIE PLIKU:
+		try 
+		{
+			fis.close();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
 		}
 		
 		return tablica;
 	}
-	
 }
