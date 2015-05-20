81b601d5ce34d0e466800d9cc9e20592fcc91745
Polankiewicz <damianpol2@o2.pl>
2015-05-20 12:42:05 +0200

    zad 5: tests

--- Zadanie5/Zadanie5/src/test/java/zad5/Zadanie5/ReadWriteTest.java
+++ Zadanie5/Zadanie5/src/test/java/zad5/Zadanie5/ReadWriteTest.java
@@ -1,18 +1,16 @@
 package zad5.Zadanie5;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 import org.junit.Test;
 
 import zad2.Zadanie2.*;
 
 public class ReadWriteTest {
-
-	private static Logger logger = Logger.getLogger(ReadWrite.class.getName());
 
 	@Test
 	public void testReadWriteText() throws Exception {
@@ -24,24 +22,24 @@ public class ReadWriteTest {
 		assertEquals(odczyt, zapis);
 	}
 
-	@Test(expected=AssertionError.class)
+	@Test
 	public void testReadWriteTextEx() throws Exception {
 		String zapis = "Zapisanie tekstu do pliku \nęóąśłżźćń\n";
 		ReadWrite.writeToTextFile(zapis, "plik2.txt");
 
 		String odczyt = ReadWrite.readFromTextFile("plik3.txt");
 
-		assertEquals(odczyt, zapis);
+		assertFalse(odczyt.equals(zapis));
 	}
 
-	@Test(expected=AssertionError.class)
+	@Test
 	public void testReadWriteTextExc() throws Exception {
 		String zapis = "Jakiś \ninny\ntekst";
 		ReadWrite.writeToTextFile(zapis, "plik2.txt");
 
 		String odczyt = ReadWrite.readFromTextFile("plik.txt");
-
-		assertEquals(odczyt, zapis);
+		
+		assertFalse(odczyt.equals(zapis));
 	}
 
 	@Test
@@ -50,6 +48,15 @@ public class ReadWriteTest {
 
 		ReadWrite.writeArrayToFile(tab, "tablice.bin");
 		Arrays.equals(tab, ReadWrite.readArrayFromFile("tablice.bin"));
+	}
+	
+	@Test
+	public void testReadWriteArrayEx() throws Exception {
+		int[] tab = Tablice.getTabWithRandValues(12);
+		ReadWrite.writeArrayToFile(tab, "tablice.bin");
+		
+		int[] tab2 = ReadWrite.readArrayFromFile("tablice999.bin");
+		assertFalse(tab.equals(tab2));
 	}
 
 	@Test
